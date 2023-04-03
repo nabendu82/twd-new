14a6466d685e517c1d41c6d2ec74d9f83301e106
tech-bhargava <techbhargava@gmail.com>
2023-04-03 16:57:31 +0530

    YT April Schedule Updated

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Tooltip as ReactTooltip } from 'react-tooltip';
 
 const Table = () => {
-  // var day04 = "Redux Crash Course at 9am-10am - {NABENDU}";
-  // var day4 = "WORKSHOP On Zoom at 3pm-6pm - {TEAM}";
+  var day026 = "React Native Beginner to Advance at 5pm-6pm - {NABENDU}";
+  var day26 = "Career doubt session - 8:30pm to 9:30pm - {NABENDU}";
   return (
     <div>
         {/* <h3 className="month_name">January - 2023</h3> */}
@@ -13,13 +13,12 @@ const Table = () => {
                 <tr style={{backgroundColor:"green",color:"#fff"}}><td style={{backgroundColor:"#f0e86c"}}></td><td style={{backgroundColor:"red",color:"#fff"}}>Sun</td><td style={{width:"70px"}}>Mon</td><td style={{width:"70px"}}>Tue</td><td style={{width:"70px"}}>Wed</td><td style={{width:"70px"}}>Thu</td><td style={{width:"70px"}}>Fri</td><td style={{width:"70px"}}>Sat</td></tr>
             </thead>
             <tbody>
-                <tr style={{backgroundColor:"#213343",color:"#fff"}}><td className="weeks">WEEK-1</td><td id="feb_26">26</td><td id="feb_27">27</td><td id="feb_28">28</td><td id="my-element_1">1</td><td id="my-element_2">2</td><td id="my-element_3">3</td><td id="my-element_4">4</td></tr>
-                <tr style={{backgroundColor:"#213343",color:"#fff"}}><td className="weeks">WEEK-2</td><td id="my-element_5">5</td><td id="my-element_6">6</td><td id="my-element_7">7</td><td id="my-element_8">8</td><td id="my-element_9">9</td><td id="my-element_10">10</td><td id="my-element_11">11</td></tr>
-                <tr style={{backgroundColor:"#213343",color:"#fff"}}><td className="weeks">WEEK-3</td><td id="my-element_12">12</td><td id="my-element_13">13</td><td id="my-element_14">14</td><td id="my-element_15">15</td><td id="my-element_16">16</td><td id="my-element_17">17</td><td id="my-element_18">18</td></tr>
-                <tr style={{backgroundColor:"#213343",color:"#fff"}}><td className="weeks">WEEK-4</td><td id="my-element_19">19</td><td id="my-element_20">20</td><td id="my-element_21">21</td><td id="my-element_22">22</td><td id="my-element_23">23</td><td id="my-element_24">24</td><td id="my-element_25">25</td></tr>
-                <tr style={{backgroundColor:"#213343",color:"#fff"}}><td className="weeks">WEEK-5</td><td id="my-element_26">26</td><td id="my-element_27">27</td><td id="my-element_28">28</td><td id="my-element_29">29</td><td id="my-element_30">30</td><td id="my-element_31">31</td><td id="my-element_32">-</td></tr>
-                {/* <tr style={{backgroundColor:"#213343",color:"#fff"}}><td id="my-element_5">6</td><td id="my-element_10">13</td><td id="my-element_15">20</td><td id="my-element_20">27</td><td id="my-element_20">-</td></tr>
-                <tr style={{backgroundColor:"#213343",color:"#fff"}}><td id="my-element_5">7</td><td id="my-element_10">14</td><td id="my-element_15">21</td><td id="my-element_20">28</td><td id="my-element_20">-</td></tr> */}
+                <tr style={{backgroundColor:"#213343",color:"#fff"}}><td className="weeks">WEEK-1</td><td id="mar_26">26</td><td id="mar_27">27</td><td id="mar_28">28</td><td id="mar_29">29</td><td id="mar_30">30</td><td id="mar_31">31</td><td id="my-element_1">1</td></tr>
+                <tr style={{backgroundColor:"#213343",color:"#fff"}}><td className="weeks">WEEK-2</td><td id="my-element_2">2</td><td id="my-element_3">3</td><td id="my-element_4">4</td><td id="my-element_5">5</td><td id="my-element_6">6</td><td id="my-element_7">7</td><td id="my-element_8">8</td></tr>
+                <tr style={{backgroundColor:"#213343",color:"#fff"}}><td className="weeks">WEEK-3</td><td id="my-element_9">9</td><td id="my-element_10">10</td><td id="my-element_11">11</td><td id="my-element_12">12</td><td id="my-element_13">13</td><td id="my-element_14">14</td><td id="my-element_15">15</td></tr>
+                <tr style={{backgroundColor:"#213343",color:"#fff"}}><td className="weeks">WEEK-4</td><td id="my-element_16">16</td><td id="my-element_17">17</td><td id="my-element_18">18</td><td id="my-element_19">19</td><td id="my-element_20">20</td><td id="my-element_21">21</td><td id="my-element_22">22</td></tr>
+                <tr style={{backgroundColor:"#213343",color:"#fff"}}><td className="weeks">WEEK-5</td><td id="my-element_23">23</td><td id="my-element_24">24</td><td id="my-element_25">25</td><td id="my-element_26">26</td><td id="my-element_27">27</td><td id="my-element_28">28</td><td id="my-element_29">29</td></tr>
+                <tr style={{backgroundColor:"#213343",color:"#fff"}}><td className="weeks">WEEK_6</td><td id="my-element_30">30</td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
                 
             </tbody>
         </table>
@@ -30,50 +29,52 @@ const Table = () => {
          1-jun
         </p> */}
         {/* WEEK-1 */}
-        <ReactTooltip anchorId="feb_26" content="-" style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="feb_27" content="CSS Beginner at 5pm-6pm - {MOUSAM}"  style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="feb_28" content="CSS Beginner at 5pm-6pm - {MOUSAM}" style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_1" content="HOLA JS at 8:30pm-9:30pm - {HARI}"  style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_2" content="-" style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_3" content="-" style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_4"  content="4Hrs WORKSHOP - HTML,CSS,JAVASCRIPT and English at 3pm-7pm - {TEAM}" place="left" style={{backgroundColor:"green",fontSize:"20px",width:"300px"}}/>
+        <ReactTooltip anchorId="mar_26" content="-" style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="mar_27" content="CSS Beginner at 5pm-6pm - {MOUSAM}"  style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="mar_28" content="CSS Beginner at 5pm-6pm - {MOUSAM}" style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="mar_29" content="HOLA JS at 8:30pm-9:30pm - {HARI}"  style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="mar_30" content="-" style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="mar_31" content="-" style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="my-element_1"  content="-" place="left" style={{backgroundColor:"green",fontSize:"20px",width:"20px"}}/>
        
         {/* WEEK-2 */}
-        <ReactTooltip anchorId="my-element_5" content="Interview Questions - HTML, CSS and JAVASCRIPT at 9am-11am - {NABENDU}"  style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_6" content="JS Beginner to Advance at 7pm-8pm  - {HARI}"  style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_7" content="-" style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_8" content="HOLA JS at 8:30pm-9:30pm - {HARI}" style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_9" content="-" style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_10" content="-" style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_11" content="4Hrs BOOTCAMP - HTML,CSS,JS with Projects at 3pm-7pm - {TEAM}" place="left" style={{backgroundColor:"green",fontSize:"20px",width:"300px"}}/>
+        <ReactTooltip anchorId="my-element_2" content="-"  style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="my-element_3" content="JS Beginner to Advance at 8:30pm-9:30pm  - {HARI}"  style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="my-element_4" content="JS Beginner to Advance at 8:30pm-9:30pm  - {HARI}" style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="my-element_5" content="Career doubt session - 8:30pm to 9:30pm - {Nabendu}" style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="my-element_6" content="JS Beginner to Advance at 8:30pm-9:30pm  - {HARI}" style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="my-element_7" content="JS Beginner to Advance at 8:30pm-9:30pm  - {HARI}" style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="my-element_8" content="4Hrs BOOTCAMP - HTML,CSS,JS with Projects at 3pm to 7pm - {TEAM}" place="left" style={{backgroundColor:"green",fontSize:"20px",width:"300px"}}/>
        
         {/* WEEK-3 */}
-        <ReactTooltip anchorId="my-element_12" content="Interview Questions - HTML, CSS and JAVASCRIPT at 9am-11am - {NABENDU}" style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_13" content="React Beginner to Advance at 7pm-8pm - {HARI}"  style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_14" content="-" style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_15" content="HOLA JS at 8:30pm-9:30pm - {HARI}" style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_16" content="-" style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_17" content="-" style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_18" content="4Hrs WORKSHOP - HTML,CSS,JS with projects at 3pm-7pm - {TEAM}" place="left" style={{backgroundColor:"green",fontSize:"20px",width:"300px"}}/>
+        <ReactTooltip anchorId="my-element_9" content="Interview Questions(Part-5) - HTML, CSS and JAVASCRIPT at 9am-10am - {NABENDU}" place="top" style={{backgroundColor:"green",fontSize:"20px", width:"300px"}}/>
+        <ReactTooltip anchorId="my-element_10" content="CSS Beginners to Advanced at 4pm-5pm - {MOUSAM}"  style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="my-element_11" content="CSS Beginners to Advanced at 4pm-5pm - {MOUSAM}" style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="my-element_12" content="Career doubt session - 8:30pm to 9:30pm - {Nabendu}" style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="my-element_13" content="CSS Beginners to Advanced at 4pm-5pm - {MOUSAM}" style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="my-element_14" content="CSS Beginners to Advanced at 4pm-5pm - {MOUSAM}" style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="my-element_15" content="4Hrs WORKSHOP - HTML,CSS,JS and Communication Skills at 3pm to 7pm - {TEAM}" place="left" style={{backgroundColor:"green",fontSize:"20px",width:"300px"}}/>
       
        {/* WEEK-4 */}
-        <ReactTooltip anchorId="my-element_19" content="Interview Questions - ReactJS at 9am-11am - {NABENDU}" style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_20" content="Bootstrap Advance to Master at 5pm-6pm - {MOUSAM}" style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_21" content="-" style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_22" content="HOLA JS at 8:30pm-9:30pm - {HARI}" style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_23" content="-" style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_24" content="-" style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_25" content="12Hrs BOOTCAMP - HTML,CSS,JS and ENGLISH at 9am-9pm - {TEAM}" place="left" style={{backgroundColor:"green",fontSize:"20px",width:"300px"}}/>
+        <ReactTooltip anchorId="my-element_16" content="Google Drive Clone in ReactJS at 9am-10am - {NABENDU}"  style={{backgroundColor:"green",fontSize:"20px", width:"300px"}}/>
+        <ReactTooltip anchorId="my-element_17" content="ReactJS Beginners to Advanced at 8:30pm-9:30pm - {HARI}" style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="my-element_18" content="ReactJS Beginners to Advanced at 8:30pm-9:30pm - {HARI}" style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="my-element_19" content="Career doubt session - 8:30pm to 9:30pm - {Nabendu}" style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="my-element_20" content="ReactJS Beginners to Advanced at 8:30pm-9:30pm - {HARI}" style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="my-element_21" content="ReactJS Beginners to Advanced at 8:30pm-9:30pm - {HARI}" style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="my-element_22" content="12Hrs BOOTCAMP - HTML,CSS,JS and Projects at 9am to 9pm - {TEAM}" place="left" style={{backgroundColor:"green",fontSize:"20px",width:"300px"}}/>
          
       {/* WEEK-5 */}
-        <ReactTooltip anchorId="my-element_26" content="-" style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_27" content="React Native Beginner at 8:30pm-9:30pm - {NABENDU}" style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_28" content="-"  style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_29" content="-" style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_30" content="-" style={{backgroundColor:"green",fontSize:"20px"}}/>
-        <ReactTooltip anchorId="my-element_31" content="-" style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="my-element_23" content="-" style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="my-element_24" content="React Native Beginners to Advanced at 5pm-6pm - {NABENDU}" style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="my-element_25" content="React Native Beginners to Advanced at 5pm-6pm - {NABENDU}"  style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="my-element_26" content= {day026 + " " + day26} style={{backgroundColor:"green",fontSize:"20px", width:"600px"}}/>
+        <ReactTooltip anchorId="my-element_27" content="React Native Beginners to Advanced at 5pm-6pm - {NABENDU}" style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="my-element_28" content="React Native Beginners to Advanced at 5pm-6pm - {NABENDU}" style={{backgroundColor:"green",fontSize:"20px"}}/>
          {/* ---- */}
-        <ReactTooltip anchorId="my-element_32" content="-" style={{backgroundColor:"green",fontSize:"20px"}}/>
+        <ReactTooltip anchorId="my-element_29" content="4Hrs WORKSHOP - HTML, CSS, JavaScript and ReactJS - 3pm to 7pm - {TEAM}" place="left" style={{backgroundColor:"green",fontSize:"20px", width:"300px"}}/>
+        <ReactTooltip anchorId="my-element_30" content="AWS S3 tutorials for Beginners - 9am to 10am - {NABENDU}" style={{backgroundColor:"green",fontSize:"20px", width:"300px"}}/>
+        
    </div>
   )
 }
